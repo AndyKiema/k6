54fef03ccfcb4160ac40cb9f60ce787e2b6894ad
Andrew Kiema <andrewkiema94@gmail.com>
2025-01-15 15:55:50 +0300

    Perform load test

---
+++ loadtest.js
@@ -0,0 +1,29 @@
+import http from 'k6/http';
+import{sleep} from 'k6';
+
+export const options={
+    stages: [
+        {
+            duration: '10s',    //duration- how long the stage lasts
+            target: 10          //tagret- number of VUs active in this stage
+        },
+        {
+            duration: '30s',
+            target: 10
+        },
+        {
+            duration: '10s',
+            target: 0
+        }
+    ]
+
+}
+
+export default function(){
+    http.get('https://test.k6.io');
+    sleep(1);
+    http.get('https://test.k6.io/contacts.php');
+    sleep(2);
+    http.get('https://test.k6.io/news.php');
+    sleep(2);
+}
