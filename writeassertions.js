a5de0b7a46494fef3839808404f2df696640a465
Andrew Kiema <andrewkiema94@gmail.com>
2025-01-16 15:35:54 +0300

    write assertions

---
+++ writeassertions.js
@@ -0,0 +1,12 @@
+import http from 'k6/http';
+import {check} from 'k6';
+
+export default function(){
+    const res=http.get('https://test.k6.io');
+    check(false, {
+        'false is false': (value)=>value===false
+    });
+    check(true, {
+        'true is true': (value)=>value===true
+    });
+}
