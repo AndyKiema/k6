24f59b2ed37250a12a7abbdbe3e741ec527424d1
Andrew Kiema <andrewkiema94@gmail.com>
2025-01-18 19:09:42 +0300

    Add metrics

--- metrics.js
+++ metrics.js
@@ -1,5 +1,18 @@
 import http from 'k6/http';
 import {check} from 'k6';
+import {sleep} from 'k6';
+
+export const options={
+  vus: 10,
+  duration: '10s',
+  thresholds: {
+    http_req_duration: ['p(95)<500'],
+    http_req_failed: ['rate<0.01'],
+    http_reqs: ['count>20'], //total number of requests made
+    http_reqs: ['rate>30'], //verifies that number of requests per second are more than 30
+    vus: ['value>9']//verifies that number of virtual users are more than 9
+  }
+}
 
 
 export default function(){
