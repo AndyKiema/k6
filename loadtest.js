27f818c72c123f3898f635d8c3ae202f2829222a
Andrew Kiema <andrewkiema94@gmail.com>
2025-05-06 12:07:03 +0300

    Add notes

--- loadtest.js
+++ loadtest.js
@@ -22,8 +22,4 @@ export const options={
 export default function(){
     http.get('https://test.k6.io');
     sleep(1);
-    http.get('https://test.k6.io/contacts.php');
-    sleep(2);
-    http.get('https://test.k6.io/news.php');
-    sleep(2);
 }
